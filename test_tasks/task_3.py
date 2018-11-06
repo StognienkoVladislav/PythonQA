2b9daac7bf0a3917526b2932152ca2e9c620c630
StognienkoVladyslav <stognienkovv@gmail.com>
2018-11-06 20:45:48 +0200

    change titles

--- test_tasks/task_3.py
+++ test_tasks/task_3.py
@@ -1,13 +1,13 @@
 
-def same_structure_as(np1, np2):
-    if len(np1) != len(np2):
+def same_structure_as(structure_1, structure_2):
+    if len(structure_1) != len(structure_2):
         return False
 
-    for lst1, lst2 in zip(np1, np2):
-        if type(lst1) != type(lst2):
+    for internal_1, internal_2 in zip(structure_1, structure_2):
+        if type(internal_1) != type(internal_2):
             return False
 
         # т.к. типы 2 элементов одинаковые, можно сравнить только 1
-        elif type(lst1) is list:
-            return same_structure_as(lst1, lst2)
+        elif type(internal_1) is list:
+            return same_structure_as(internal_1, internal_2)
     return True
